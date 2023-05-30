e9c0d68cf968ae6b98ce8c8699bec369c16ab74e
saturnesegithub <134809658+saturnesegithub@users.noreply.github.com>
2023-05-30 20:56:12 +0100

    Create 3by3version.py

---
+++ 3by3version.py
@@ -0,0 +1,775 @@
+import random
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def UL():
+
+    def r1():
+
+        subr1dat1 = random.choice(nums)
+
+        subr1dat2 = random.choice(nums)
+
+        subr1dat3 = random.choice(nums)
+
+        return [subr1dat1, subr1dat2, subr1dat3]
+
+    def r2():
+
+        subr2dat1 = random.choice(nums)
+
+        subr2dat2 = random.choice(nums)
+
+        subr2dat3 = random.choice(nums)
+
+        return [subr2dat1, subr2dat2, subr2dat3]
+
+    def r3():
+
+        subr3dat1 = random.choice(nums)
+
+        subr3dat2 = random.choice(nums)
+
+        subr3dat3 = random.choice(nums)
+
+        return [subr3dat1, subr3dat2, subr3dat3]
+
+    def finderrors():
+
+        if r1dat[1] in r2dat:
+
+            r2dat[1] = "find"
+
+        if r1dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r1dat[2] in r2dat:
+
+            r2dat[2] = "find"
+
+        if r1dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        if r1dat[0] in r2dat:
+
+            r2dat[0] = "find"
+
+        if r1dat[0] in r3dat:
+
+            r3dat[0] = "find"
+
+        if r2dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r2dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        return r1dat, r2dat, r3dat
+
+    r1dat = r1()
+
+    r2dat = r2()
+
+    r3dat = r3()
+
+    wholedata = [r1dat, r2dat, r3dat]
+
+    return finderrors()
+
+def UC():
+
+    def r1():
+
+        subr1dat1 = random.choice(nums)
+
+        subr1dat2 = random.choice(nums)
+
+        subr1dat3 = random.choice(nums)
+
+        return [subr1dat1, subr1dat2, subr1dat3]
+
+    def r2():
+
+        subr2dat1 = random.choice(nums)
+
+        subr2dat2 = random.choice(nums)
+
+        subr2dat3 = random.choice(nums)
+
+        return [subr2dat1, subr2dat2, subr2dat3]
+
+    def r3():
+
+        subr3dat1 = random.choice(nums)
+
+        subr3dat2 = random.choice(nums)
+
+        subr3dat3 = random.choice(nums)
+
+        return [subr3dat1, subr3dat2, subr3dat3]
+
+    def finderrors():
+
+        if r1dat[1] in r2dat:
+
+            r2dat[1] = "find"
+
+        if r1dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r1dat[2] in r2dat:
+
+            r2dat[2] = "find"
+
+        if r1dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        if r1dat[0] in r2dat:
+
+            r2dat[0] = "find"
+
+        if r1dat[0] in r3dat:
+
+            r3dat[0] = "find"
+
+        if r2dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r2dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        return r1dat, r2dat, r3dat
+
+    r1dat = r1()
+
+    r2dat = r2()
+
+    r3dat = r3()
+
+    wholedata = [r1dat, r2dat, r3dat]
+
+    return finderrors()
+
+def UR():
+
+    def r1():
+
+        subr1dat1 = random.choice(nums)
+
+        subr1dat2 = random.choice(nums)
+
+        subr1dat3 = random.choice(nums)
+
+        return [subr1dat1, subr1dat2, subr1dat3]
+
+    def r2():
+
+        subr2dat1 = random.choice(nums)
+
+        subr2dat2 = random.choice(nums)
+
+        subr2dat3 = random.choice(nums)
+
+        return [subr2dat1, subr2dat2, subr2dat3]
+
+    def r3():
+
+        subr3dat1 = random.choice(nums)
+
+        subr3dat2 = random.choice(nums)
+
+        subr3dat3 = random.choice(nums)
+
+        return [subr3dat1, subr3dat2, subr3dat3]
+
+    def finderrors():
+
+        if r1dat[1] in r2dat:
+
+            r2dat[1] = "find"
+
+        if r1dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r1dat[2] in r2dat:
+
+            r2dat[2] = "find"
+
+        if r1dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        if r1dat[0] in r2dat:
+
+            r2dat[0] = "find"
+
+        if r1dat[0] in r3dat:
+
+            r3dat[0] = "find"
+
+        if r2dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r2dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        return r1dat, r2dat, r3dat
+
+    r1dat = r1()
+
+    r2dat = r2()
+
+    r3dat = r3()
+
+    wholedata = [r1dat, r2dat, r3dat]
+
+    return finderrors()
+
+def CL():
+
+    def r1():
+
+        subr1dat1 = random.choice(nums)
+
+        subr1dat2 = random.choice(nums)
+
+        subr1dat3 = random.choice(nums)
+
+        return [subr1dat1, subr1dat2, subr1dat3]
+
+    def r2():
+
+        subr2dat1 = random.choice(nums)
+
+        subr2dat2 = random.choice(nums)
+
+        subr2dat3 = random.choice(nums)
+
+        return [subr2dat1, subr2dat2, subr2dat3]
+
+    def r3():
+
+        subr3dat1 = random.choice(nums)
+
+        subr3dat2 = random.choice(nums)
+
+        subr3dat3 = random.choice(nums)
+
+        return [subr3dat1, subr3dat2, subr3dat3]
+
+    def finderrors():
+
+        if r1dat[1] in r2dat:
+
+            r2dat[1] = "find"
+
+        if r1dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r1dat[2] in r2dat:
+
+            r2dat[2] = "find"
+
+        if r1dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        if r1dat[0] in r2dat:
+
+            r2dat[0] = "find"
+
+        if r1dat[0] in r3dat:
+
+            r3dat[0] = "find"
+
+        if r2dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r2dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        return r1dat, r2dat, r3dat
+
+    r1dat = r1()
+
+    r2dat = r2()
+
+    r3dat = r3()
+
+    wholedata = [r1dat, r2dat, r3dat]
+
+    return finderrors()
+
+def CC():
+
+    def r1():
+
+        subr1dat1 = random.choice(nums)
+
+        subr1dat2 = random.choice(nums)
+
+        subr1dat3 = random.choice(nums)
+
+        return [subr1dat1, subr1dat2, subr1dat3]
+
+    def r2():
+
+        subr2dat1 = random.choice(nums)
+
+        subr2dat2 = random.choice(nums)
+
+        subr2dat3 = random.choice(nums)
+
+        return [subr2dat1, subr2dat2, subr2dat3]
+
+    def r3():
+
+        subr3dat1 = random.choice(nums)
+
+        subr3dat2 = random.choice(nums)
+
+        subr3dat3 = random.choice(nums)
+
+        return [subr3dat1, subr3dat2, subr3dat3]
+
+    def finderrors():
+
+        if r1dat[1] in r2dat:
+
+            r2dat[1] = "find"
+
+        if r1dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r1dat[2] in r2dat:
+
+            r2dat[2] = "find"
+
+        if r1dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        if r1dat[0] in r2dat:
+
+            r2dat[0] = "find"
+
+        if r1dat[0] in r3dat:
+
+            r3dat[0] = "find"
+
+        if r2dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r2dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        return r1dat, r2dat, r3dat
+
+    r1dat = r1()
+
+    r2dat = r2()
+
+    r3dat = r3()
+
+    wholedata = [r1dat, r2dat, r3dat]
+
+    return finderrors()
+
+def CR():
+
+    def r1():
+
+        subr1dat1 = random.choice(nums)
+
+        subr1dat2 = random.choice(nums)
+
+        subr1dat3 = random.choice(nums)
+
+        return [subr1dat1, subr1dat2, subr1dat3]
+
+    def r2():
+
+        subr2dat1 = random.choice(nums)
+
+        subr2dat2 = random.choice(nums)
+
+        subr2dat3 = random.choice(nums)
+
+        return [subr2dat1, subr2dat2, subr2dat3]
+
+    def r3():
+
+        subr3dat1 = random.choice(nums)
+
+        subr3dat2 = random.choice(nums)
+
+        subr3dat3 = random.choice(nums)
+
+        return [subr3dat1, subr3dat2, subr3dat3]
+
+    def finderrors():
+
+        if r1dat[1] in r2dat:
+
+            r2dat[1] = "find"
+
+        if r1dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r1dat[2] in r2dat:
+
+            r2dat[2] = "find"
+
+        if r1dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        if r1dat[0] in r2dat:
+
+            r2dat[0] = "find"
+
+        if r1dat[0] in r3dat:
+
+            r3dat[0] = "find"
+
+        if r2dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r2dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        return r1dat, r2dat, r3dat
+
+    r1dat = r1()
+
+    r2dat = r2()
+
+    r3dat = r3()
+
+    wholedata = [r1dat, r2dat, r3dat]
+
+    return finderrors()
+
+def DL():
+
+    def r1():
+
+        subr1dat1 = random.choice(nums)
+
+        subr1dat2 = random.choice(nums)
+
+        subr1dat3 = random.choice(nums)
+
+        return [subr1dat1, subr1dat2, subr1dat3]
+
+    def r2():
+
+        subr2dat1 = random.choice(nums)
+
+        subr2dat2 = random.choice(nums)
+
+        subr2dat3 = random.choice(nums)
+
+        return [subr2dat1, subr2dat2, subr2dat3]
+
+    def r3():
+
+        subr3dat1 = random.choice(nums)
+
+        subr3dat2 = random.choice(nums)
+
+        subr3dat3 = random.choice(nums)
+
+        return [subr3dat1, subr3dat2, subr3dat3]
+
+    def finderrors():
+
+        if r1dat[1] in r2dat:
+
+            r2dat[1] = "find"
+
+        if r1dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r1dat[2] in r2dat:
+
+            r2dat[2] = "find"
+
+        if r1dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        if r1dat[0] in r2dat:
+
+            r2dat[0] = "find"
+
+        if r1dat[0] in r3dat:
+
+            r3dat[0] = "find"
+
+        if r2dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r2dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        return r1dat, r2dat, r3dat
+
+    r1dat = r1()
+
+    r2dat = r2()
+
+    r3dat = r3()
+
+    wholedata = [r1dat, r2dat, r3dat]
+
+    return finderrors()
+
+def DC():
+
+    def r1():
+
+        subr1dat1 = random.choice(nums)
+
+        subr1dat2 = random.choice(nums)
+
+        subr1dat3 = random.choice(nums)
+
+        return [subr1dat1, subr1dat2, subr1dat3]
+
+    def r2():
+
+        subr2dat1 = random.choice(nums)
+
+        subr2dat2 = random.choice(nums)
+
+        subr2dat3 = random.choice(nums)
+
+        return [subr2dat1, subr2dat2, subr2dat3]
+
+    def r3():
+
+        subr3dat1 = random.choice(nums)
+
+        subr3dat2 = random.choice(nums)
+
+        subr3dat3 = random.choice(nums)
+
+        return [subr3dat1, subr3dat2, subr3dat3]
+
+    def finderrors():
+
+        if r1dat[1] in r2dat:
+
+            r2dat[1] = "find"
+
+        if r1dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r1dat[2] in r2dat:
+
+            r2dat[2] = "find"
+
+        if r1dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        if r1dat[0] in r2dat:
+
+            r2dat[0] = "find"
+
+        if r1dat[0] in r3dat:
+
+            r3dat[0] = "find"
+
+        if r2dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r2dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        return r1dat, r2dat, r3dat
+
+    r1dat = r1()
+
+    r2dat = r2()
+
+    r3dat = r3()
+
+    wholedata = [r1dat, r2dat, r3dat]
+
+    return finderrors()
+
+def DR():
+
+    def r1():
+
+        subr1dat1 = random.choice(nums)
+
+        subr1dat2 = random.choice(nums)
+
+        subr1dat3 = random.choice(nums)
+
+        return [subr1dat1, subr1dat2, subr1dat3]
+
+    def r2():
+
+        subr2dat1 = random.choice(nums)
+
+        subr2dat2 = random.choice(nums)
+
+        subr2dat3 = random.choice(nums)
+
+        return [subr2dat1, subr2dat2, subr2dat3]
+
+    def r3():
+
+        subr3dat1 = random.choice(nums)
+
+        subr3dat2 = random.choice(nums)
+
+        subr3dat3 = random.choice(nums)
+
+        return [subr3dat1, subr3dat2, subr3dat3]
+
+    def finderrors():
+
+        if r1dat[1] in r2dat:
+
+            r2dat[1] = "find"
+
+        if r1dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r1dat[2] in r2dat:
+
+            r2dat[2] = "find"
+
+        if r1dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        if r1dat[0] in r2dat:
+
+            r2dat[0] = "find"
+
+        if r1dat[0] in r3dat:
+
+            r3dat[0] = "find"
+
+        if r2dat[1] in r3dat:
+
+            r3dat[1] = "find"
+
+        if r2dat[2] in r3dat:
+
+            r3dat[2] = "find"
+
+        if r2dat[0] in r3dat:
+
+            r3dat[0] = "find"
+
+        return r1dat, r2dat, r3dat
+
+    r1dat = r1()
+
+    r2dat = r2()
+
+    r3dat = r3()
+
+    wholedata = [r1dat, r2dat, r3dat]
+
+    return finderrors()
+
+def output():
+
+    print("Top/Up Rows: \n")
+
+    print("Left Block: \n")
+
+    print(UL(),"\n")
+
+    print("Middle Block: \n")
+
+    print(UC(),"\n")
+
+    print("Right Block: \n")
+
+    print(UR(),"\n")
+
+    print("Center/Middle Rows: \n")
+
+    print("Left Block: \n")
+
+    print(CL(),"\n")
+
+    print("Middle Block: \n")
+
+    print(CC(),"\n")
+
+    print("Right Block: \n")
+
+    print(CR(),"\n")
+
+    print("\n")
+
+    print("Bottom/Down Rows: \n")
+
+    print("Left Block: \n")
+
+    print(DL(),"\n")
+
+    print("Middle Block: \n")
+
+    print(DC(),"\n")
+
+    print("Right Block: \n")
+
+    print(DR(),"\n")
+
+    print("\n")
+
+    print("How it works:")
+
+    print("Lets say you have [(1,2,3),(4,5,6),(7,8,9)]")
+
+    print("Then,ets say Top Left block would look like this:")
+
+    print("[1|2|3]")
+
+    print("[4|5|6]")
+
+    print("[7|8|9]")
+
+    print("\n")
+
+    print("Same Applies to every Block")
+
+output()
